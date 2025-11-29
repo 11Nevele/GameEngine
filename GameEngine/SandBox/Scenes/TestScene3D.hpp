dd67e50f343a7a04fa2e1fde92891f5f376a1b8b
Nickwsy <wsynick@163.com>
2025-11-29 16:48:41 -0500

    Clean up and added assimp

---
+++ GameEngine/SandBox/Scenes/TestScene3D.hpp
@@ -0,0 +1,86 @@
+#pragma once
+#include "acpch.h"
+#include "Achoium.h"
+
+using namespace std;
+using namespace ac;
+
+using mTexture2D = OpenGLTexture2D;
+using mInput = WindowsInput;
+using mWindow = WinWindow;
+using mRenderer = OpenGLRenderer;
+
+
+
+bool exitgame = false;
+
+bool HandelGameExit(const WindowCloseEvent& event)
+{
+	exitgame = true;
+	ACMSG("Game exit requested.");
+	return true;
+}
+ac::World world;
+bool HandleWindowResize(const WindowResizeEvent& event)
+{
+	ACMSG("Window resized: " << event.width << " x " << event.height);
+
+	// Notify renderer to adjust viewport size
+	mRenderer& renderer = world.GetResourse<mRenderer>();
+	renderer.OnWindowResize(event.width, event.height);
+
+	// If your game logic needs to know the window size, you can update it here
+
+	return true; // Return true to indicate the event has been handled
+}
+
+void Movement(ac::World& world)
+{
+
+	
+}
+
+
+void LoadAssets(World& world)
+{
+	std::string curPath = filesystem::current_path().string();
+	world.GetResourse<TextureManager>().AddTexture("White", curPath + "/Assets/Image/White.png");
+}
+
+void TestScene3D()
+{
+
+
+	srand(time(0));
+	InitEngine(world);
+
+	Entity camera = world.CreateEntity();
+
+
+	world.Add<Camera>(camera, Camera{ });
+	world.Add<AudioListener>(camera, AudioListener());
+	world.Add<Transform>(camera, Transform());
+	world.Get<Transform>(camera).position = { 0, 0, 0 };
+
+	LoadAssets(world);
+
+	Entity tilemap = world.CreateEntity();
+	const int width = 10, height = 10;
+	world.Add<ac::Tilemap>(tilemap, ac::Tilemap(width, height, 40, 40));
+
+	
+	while (true)
+	{
+		mWindow& win = world.GetResourse<mWindow>();
+
+		mRenderer& renderer = world.GetResourse<mRenderer>();
+
+		world.Update();
+		if (exitgame)
+			break;
+
+		win.OnUpdate();
+		glClearColor(0.1, 0.1, 0.1, 1);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
